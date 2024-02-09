2c6e62690cd40f8362aa649b991a99b6cc4371e7
chandani34 <uiuxdesign34@gmail.com>
2024-02-09 15:11:29 +0530

    Version 1

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+function App() {
+ 
+
+  return (
+    <div>
+      <Chai/>
+      <h3>Waiting for react advance learning</h3>
+      <span>scan the qr code</span>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,10 @@
+function Chai(){
+    return(
+        <h2>chai is ready</h2>,
+        <div>this is first page of my react application , here i am using vite react library for creating project
+
+        </div>
+    )
+}
+export default Chai
+
